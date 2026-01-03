29544fbf4f6a650b9d73d08352232d7d9f1aab4a
NH-Bappy <naimulbappy2207@gmail.com>
2026-01-03 19:29:26 +0600

    add virtualizer

--- src/components/dashboard/product/ProductList.jsx
+++ src/components/dashboard/product/ProductList.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import React from "react";
 import {
   Table,
   TableBody,
@@ -13,17 +14,21 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { getProductService } from "../../../hooks/api";
 import { Link } from "react-router";
+import { useVirtualizer } from "@tanstack/react-virtual";
 
 export default function ProductList() {
+  const parentRef = React.useRef(null);
 
+  const { data, isPending, isError, error } = getProductService("single");
 
-    const { data, isPending, isError, error, refetch } =
-    getProductService("single");
+  const products = data?.data ?? [];
 
-    // console.log(data?.data)
-
-
-
+  const rowVirtualizer = useVirtualizer({
+    count: products.length,
+    getScrollElement: () => parentRef.current,
+    estimateSize: () => 60, // row height
+    overscan: 5,
+  });
 
   return (
     <Card className="rounded-2xl shadow-sm">
@@ -32,57 +37,86 @@ export default function ProductList() {
       </CardHeader>
 
       <CardContent>
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead>Image</TableHead>
-              <TableHead>Product Name</TableHead>
-              <TableHead className="text-right">Action</TableHead>
-            </TableRow>
-          </TableHeader>
-
-          <TableBody>
-            {isPending && (
+        {/* SCROLL CONTAINER */}
+        <div
+          ref={parentRef}
+          style={{
+            height: "400px",
+            overflow: "auto",
+          }}
+        >
+          <Table>
+            <TableHeader>
               <TableRow>
-                <TableCell colSpan={3} className="text-center">
-                  Loading...
-                </TableCell>
+                <TableHead>Image</TableHead>
+                <TableHead>Product Name</TableHead>
+                <TableHead className="text-right">Action</TableHead>
               </TableRow>
-            )}
+            </TableHeader>
 
-            {isError && (
-              <TableRow>
-                <TableCell colSpan={3} className="text-center text-red-500">
-                  {error.message}
-                </TableCell>
-              </TableRow>
-            )}
+            <TableBody
+              style={{
+                height: `${rowVirtualizer.getTotalSize()}px`,
+                position: "relative",
+              }}
+            >
+              {isPending && (
+                <TableRow>
+                  <TableCell colSpan={3} className="text-center">
+                    Loading...
+                  </TableCell>
+                </TableRow>
+              )}
 
-            {data?.data?.map((product) => (
-              <TableRow key={product._id}>
-                {/* Image */}
-                <TableCell>
-                  <Avatar className="h-10 w-10 rounded-lg">
-                    <AvatarImage src={product.image} />
-                    <AvatarFallback>{product.name?.charAt(0)}</AvatarFallback>
-                  </Avatar>
-                </TableCell>
+              {isError && (
+                <TableRow>
+                  <TableCell colSpan={3} className="text-center text-red-500">
+                    {error.message}
+                  </TableCell>
+                </TableRow>
+              )}
 
-                {/* Name */}
-                <TableCell className="font-medium">{product.name}</TableCell>
+              {rowVirtualizer.getVirtualItems().map((virtualRow) => {
+                const product = products[virtualRow.index];
+                if (!product) return null;
 
-                {/* Action */}
-                <TableCell className="text-right">
-                  <Link to={`/product-details/${product.slug}`}>
-                    <Button size="sm" variant="outline">
-                      View
-                    </Button>
-                  </Link>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+                return (
+                  <TableRow
+                    key={product._id}
+                    style={{
+                      position: "absolute",
+                      top: 0,
+                      left: 0,
+                      width: "100%",
+                      transform: `translateY(${virtualRow.start}px)`,
+                    }}
+                  >
+                    <TableCell>
+                      <Avatar className="h-10 w-10 rounded-lg">
+                        <AvatarImage src={product.image} />
+                        <AvatarFallback>
+                          {product.name?.charAt(0)}
+                        </AvatarFallback>
+                      </Avatar>
+                    </TableCell>
+
+                    <TableCell className="font-medium">
+                      {product.name}
+                    </TableCell>
+
+                    <TableCell className="text-right">
+                      <Link to={`/product-details/${product.slug}`}>
+                        <Button size="sm" variant="outline">
+                          View
+                        </Button>
+                      </Link>
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </div>
       </CardContent>
     </Card>
   );
